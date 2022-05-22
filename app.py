80ce3cc006eff93f9ecca048cd66af8fb8494961
blueprintparadise <rhiray03@gmail.com>
2022-05-22 19:40:59 +0530

    Made compatable with pytorch. Further improvements needed.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import os
 import sys
 
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+
+# Images
+#img = r'C:\Users\rhira\Desktop\dlf/1.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+#results = model(img)
+
+# Results
+#results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
+
 # Flask
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
 from werkzeug.utils import secure_filename
@@ -28,18 +42,18 @@ app = Flask(__name__)
 # or https://www.tensorflow.org/api_docs/python/tf/keras/applications
 
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
+#model = MobileNetV2(weights='imagenet')
 
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
+#MODEL_PATH = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 # Load your own trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+#model = torch.load(MODEL_PATH)
+#model(img)          # Necessary
+print('Model loaded. Start serving...')
 
 
 def model_predict(img, model):
@@ -48,13 +62,13 @@ def model_predict(img, model):
     # Preprocessing the image
     x = image.img_to_array(img)
     # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
+    #x = np.expand_dims(x, axis=0)
 
     # Be careful how your trained model deals with the input
     # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
+    #x = preprocess_input(x, mode='tf')
 
-    preds = model.predict(x)
+    preds = model(x)
     return preds
 
 
@@ -77,14 +91,17 @@ def predict():
         preds = model_predict(img, model)
 
         # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+#        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+#        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
 
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
+ #       result = str(pred_class[0][0][1])               # Convert to string
+ #       result = result.replace('_', ' ').capitalize()
+        result = preds
         # Serialize the result, you can add additional fields
-        return jsonify(result=result, probability=pred_proba)
+        #jsonify(result=result)
+        result.show()
+        print(result)
+        return str(result)
 
     return None
 
